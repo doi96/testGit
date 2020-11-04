d911d95b1f942bcae9b496a8f85d8ce3135f071f
Thien Tran <thientran98qb@gmail.com>
2020-11-04 10:39:40 +0700

    new Update

--- Data.php
+++ Data.php
@@ -6,9 +6,15 @@ class Database{
     public $db;
     function __constructor(){
        try {
-         $this->db=new PDO(self::DNS,self::DBUSER);
-       } catch (\PDOExeption $th) {
-           //throw $th;
+         $this->db=new PDO(self::DNS,self::DBUSER,self::DBPASS,array(
+          PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+          PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+        ));
+        $this->db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+       } catch (PDOException $e) {
+          $error_message = $e->getMessage();
+          //include 'database_error.php';
+          exit();
        }
     }
 }
